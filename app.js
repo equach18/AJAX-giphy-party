08f70d9e039074a7e9baafe865a25b77eeba765a
Elaine Quach <equach18@gmail.com>
2024-04-18 19:33:16 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+// const gifResults = document.querySelector("#gif-results");
+console.log("Let's get this party started!");
+
+async function getGifs(searchText) {
+    const res = await axios.get("https://api.giphy.com/v1/gifs/search", {params: {api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", q: searchText}});
+    console.log(res.data);
+    return res.data;
+}
+
+function appendGif(res){
+    let numResults = res.data.length;
+    let randomNum = Math.floor(Math.random() * numResults);
+    console.log(randomNum,res)
+
+    let newImg = document.createElement("img");
+    newImg.src = res.data[randomNum].images.original.url;
+    let gifResults = document.querySelector("#gif-results");
+    gifResults.appendChild(newImg);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const form = document.querySelector('form');
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const input = document.querySelector('#search');
+        let newGif = await getGifs(input.value);
+        input.value = '';
+        appendGif(newGif);
+    });
+
+    const removeBtn = document.querySelector('#remove-btn');
+    removeBtn.addEventListener("click", (e) => {
+        // e.preventDefault();
+        let gifResults = document.querySelector("#gif-results");
+        gifResults.innerHTML = '';
+    });
+});
+
+
+
